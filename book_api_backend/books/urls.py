20299cd9340df8affb341dbc19a37c643c3ce5bb
RoshanPaulBabu <89399674+RoshanPaulBabu@users.noreply.github.com>
2024-07-13 17:44:47 +0530

    Add files via upload

---
+++ book_api_backend/books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('books/update/<int:pk>/', views.BookUpdateView.as_view(), name='book-update'),
+
+]
+
